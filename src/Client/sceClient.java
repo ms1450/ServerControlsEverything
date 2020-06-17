efc963dc5184c80dfae25260a7dcb492a55330cd
ms1450 <ms1450@rit.edu>
2020-06-17 18:46:53 +0530

    Added Comments for the Client Side of the Program as well as made a few minor changes

--- src/Client/sceClient.java
+++ src/Client/sceClient.java
@@ -1,5 +1,6 @@
 package Client;
 
+// Imports for this Class
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,50 +9,76 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class sceClient {
-    final static String IPADDRESS = "192.168.29.45";
+    //The Server IP Address
+    final static String IPADDRESS = "127.0.0.1";
+    //Port Number to establish TCP Connection on
     final static int PORTNUM = 5439;
+    //Running state of the Thread
     static boolean running = false;
 
     public static void main(String [] args) throws IOException{
+        //Establish a TCP connection with the server
         Socket socket = new Socket(IPADDRESS,PORTNUM);
-        System.err.println("Connection Established with Server on IP:"+IPADDRESS+" Port:" +PORTNUM);
+        System.out.println("Connection Established with Server on IP:"+IPADDRESS+" Port:" +PORTNUM);
+
+        //Get the OutputStream and InputStream as well as User Input Stream
         PrintWriter writer = new PrintWriter(socket.getOutputStream());
         BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         Scanner userinp = new Scanner(System.in);
 
-        System.err.println("Enter User Code: ");
+        //Get the User Code
+        System.err.print("Enter User Code: ");
         int usrcode = Integer.parseInt(userinp.nextLine());
 
+        //Send out the User Code to the Server
         writer.println("USRCODE "+usrcode);
         writer.flush();
 
+        //Read if the User Code gets Accepted or Denied
         String line = "";
         try {
             line = input.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //Server sends CNFD
         if(line.equals("CNFD")){
-            System.err.println("User Code Confirmed");
+            System.out.println("User Code Confirmed");
+
+            //Get the Recipient User Code
             System.err.println("Enter Recipient User Code: ");
             int recpusrcode = Integer.parseInt(userinp.nextLine());
 
+            //Send out Recipient User Code to the Server
             writer.println("RECP_USRCODE "+recpusrcode);
             writer.flush();
 
+            //Start the Conversation Method
             initiateConversation(writer, input, userinp);
-            socket.close();
         }
         else if(line.equals("DENY")){
+            //Server sends DENY
             System.err.println("Another Connection with the Same User Code Active");
-            System.err.println("Disconnected");
         }
+        closeConnection(socket,writer,input,userinp);
     }
 
+    //Closes the Socket and all the Data Streams
+    public static void closeConnection(Socket socket, PrintWriter output, BufferedReader input, Scanner userinput) throws IOException {
+        input.close();
+        output.close();
+        userinput.close();
+        socket.close();
+        System.out.println("Connection with IP:" + IPADDRESS + " closed Successfully.");
+    }
+
+    //Starts the reader thread as well as takes in user input and sends it to the server
     static void initiateConversation(PrintWriter output, BufferedReader input, Scanner userinp) throws IOException {
+        //Starts the sceMessageReader Thread
         Thread reader = new sceMessageReader(input);
         reader.start();
 
+        //Runs till User sends a QUIT, or the Server sends out a QUIT
         running = true;
         while(running){
             String line = userinp.nextLine();
@@ -62,8 +89,5 @@ public class sceClient {
                 System.err.println("Closing Connection to Server.");
             }
         }
-        input.close();
-        output.close();
-        System.err.println("Connection with IP:" + IPADDRESS + " closed Successfully.");
     }
 }
--- src/Client/sceMessageReader.java
+++ src/Client/sceMessageReader.java
@@ -1,17 +1,20 @@
 package Client;
 
-
+//Imports for this class
 import java.io.BufferedReader;
 import java.io.IOException;
 
+//Helper class that prints out all the messages the server is sending to the clients display
 public class sceMessageReader extends Thread {
     BufferedReader reader;
+
+    //constructor for sceMessageReader
     public sceMessageReader(BufferedReader reader){
         this.reader = reader;
     }
 
+    //Runs for the entire duration of the connection, printing out server messages
     public void run(){
-
         while(sceClient.running){
             String line = null;
             try {
@@ -19,8 +22,9 @@ public class sceMessageReader extends Thread {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            //Checks if server has sent a QUIT
             if (line != null && line.equals("QUIT")) {
-                System.err.println("Host Disconnected.");
+                System.err.println("Server Disconnected.");
                 sceClient.running = false;
             }
             if(line != null) System.out.println("> "+ line);
